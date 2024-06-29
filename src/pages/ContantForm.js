a9e8665de98eae502a2f5647048feccab2a71b47
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2024-06-29 09:55:50 +0300

    -Added a thank you message that displays after form submission. -Implemented reset of the submitted state when the "Contact Us" link is clicked.  -Improved user experience by providing feedback after form submission.  -Adjusted form validation to ensure proper input.

--- src/pages/ContantForm.js
+++ src/pages/ContantForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { ToastContainer, toast } from 'react-toastify';
@@ -7,6 +7,8 @@ import "../css/ContantForm.css";
 import featuredImage from '../img/contact.png';
 
 const ContactForm = () => {
+    const [submitted, setSubmitted] = useState(false);
+
     const validationSchema = Yup.object().shape({
         firstName: Yup.string()
             .matches(/^[\u0590-\u05FFa-zA-Z\s]+$/, 'First name should contain only English or Hebrew characters and spaces')
@@ -23,62 +25,69 @@ const ContactForm = () => {
 
     return (
         <div className="contact-form">
-            <section className="contact-form-inner">
+            <section className="contact-form-inner" style={{ width: "80%" }}>
                 <div className="card">
                     <h3 className="title">Contact us</h3>
-                    <Formik
-                        initialValues={{ firstName: '', lastName: '', email: '', message: '' }}
-                        validationSchema={validationSchema}
-                        onSubmit={(values, { resetForm }) => {
-                            const userInfo = `First Name: ${values.firstName}\nLast Name: ${values.lastName}\nEmail: ${values.email}\nMessage: ${values.message}`;
-                            console.log(userInfo)
-                            toast.success(userInfo);
-                            resetForm();
-                        }}
-                    >
-                        {({ errors, touched }) => (
-                            <Form style={{ width: '100%',display:"flex",flexDirection:"column",alignItems:"center" ,justifyContent:"center"}} noValidate>
-                                <div className= "input-container">
-                                    <Field
-                                        className={`input ${errors.firstName && touched.firstName ? 'error' : ''}`}
-                                        name="firstName"
-                                        placeholder="First name"
-                                    />
-                                    <ErrorMessage name="firstName" component="div" className="error" />
-                                </div>
-                                <div className= "input-container">
-                                    <Field
-                                        className={`input ${errors.lastName && touched.lastName ? 'error' : ''}`}
-                                        name="lastName"
-                                        placeholder="Last name"
-                                    />
-                                    <ErrorMessage name="lastName" component="div" className="error" />
-                                </div>
-                                <div className= "input-container">
-                                    <Field
-                                        className={`input ${errors.email && touched.email ? 'error' : ''}`}
-                                        name="email"
-                                        type="email"
-                                        placeholder="Email"
-                                    />
-                                    <ErrorMessage name="email" component="div" className="error" />
-                                </div>
-                                <div className= "input-container">
-                                    <Field
-                                        as="textarea"
-                                        className={`input-message ${errors.message && touched.message ? 'error' : ''}`}
-                                        name="message"
-                                        placeholder="Enter your question or message"
-                                    />
-                                    <ErrorMessage name="message" component="div" className="error" />
-                                </div>
-                                <button className="submit" type="submit">Submit</button>
-                            </Form>
-                        )}
-                    </Formik>
+                    {!submitted ? (
+                        <Formik
+                            initialValues={{ firstName: '', lastName: '', email: '', message: '' }}
+                            validationSchema={validationSchema}
+                            onSubmit={(values, { resetForm }) => {
+                                const userInfo = `First Name: ${values.firstName}\nLast Name: ${values.lastName}\nEmail: ${values.email}\nMessage: ${values.message}`;
+                                toast.success(userInfo);
+                                setSubmitted(true);
+                                resetForm();
+                            }}
+                        >
+                            {({ errors, touched }) => (
+                                <Form style={{ width: '100%', display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }} noValidate>
+                                    <div className="input-container">
+                                        <Field
+                                            className={`input ${errors.firstName && touched.firstName ? 'error' : ''}`}
+                                            name="firstName"
+                                            placeholder="First name"
+                                        />
+                                        <ErrorMessage name="firstName" component="div" className="error" />
+                                    </div>
+                                    <div className="input-container">
+                                        <Field
+                                            className={`input ${errors.lastName && touched.lastName ? 'error' : ''}`}
+                                            name="lastName"
+                                            placeholder="Last name"
+                                        />
+                                        <ErrorMessage name="lastName" component="div" className="error" />
+                                    </div>
+                                    <div className="input-container">
+                                        <Field
+                                            className={`input ${errors.email && touched.email ? 'error' : ''}`}
+                                            name="email"
+                                            type="email"
+                                            placeholder="Email"
+                                        />
+                                        <ErrorMessage name="email" component="div" className="error" />
+                                    </div>
+                                    <div className="input-container">
+                                        <Field
+                                            as="textarea"
+                                            className={`input-message ${errors.message && touched.message ? 'error' : ''}`}
+                                            name="message"
+                                            placeholder="Enter your question or message"
+                                        />
+                                        <ErrorMessage name="message" component="div" className="error" />
+                                    </div>
+                                    <button className="submit" type="submit">Submit</button>
+                                </Form>
+                            )}
+                        </Formik>
+                    ) : (
+                        <div className="thank-you-message">
+                            <h2 className="title">Thank you!</h2>
+                            <p className="title" style={{ fontSize: "28px" }}>We appreciate you contacting us. We will get back to you soon!</p>
+                        </div>
+                    )}
                 </div>
                 <div className="card">
-                    <img className="card-img-contact" src={featuredImage} alt="" />
+                    <img className="card-img-contact" src={featuredImage} alt="Contact" />
                 </div>
             </section>
             <ToastContainer />
