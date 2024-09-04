c269157d0692dadaecf270ea196531ec8d563b97
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2024-09-04 11:59:15 +0300

    Modernization for design and transition to tailwind - I changed the widths from % to vh/svh - Added scroll behavior to the NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 import '../css/NavigationBar.css';
 import '../css/App.css';
 
-const NavigationBar = ({ className = "" }) => {
+const NavigationBar = ({className = ""}) => {
+    const [isScrolling, setScrolling] = useState(false);
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >10) {
+                setScrolling(true);
+            } else {
+                setScrolling(false);
+            }
+        };
+        handleScroll();
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [])
+
     return (
-        <div className={`navigation ${className}`}>
+        <div className={`navigation ${isScrolling ? 'glassy' : ''} ${className}`}>
             <div className="mid-brand">
                 <Link to="/" className="logo-link">
                     <b className="logo">
@@ -17,7 +36,8 @@ const NavigationBar = ({ className = "" }) => {
             </div>
             <div className="right-nav-buttons">
                 <Link to="/contact" className="contact-us-link">Contact Us</Link>
-                <Link to="/auth" state={{ showRegister: false }} className="login-btn" style={{ textDecoration: 'none', height: "fit-content" }}>Login</Link>
+                <Link to="/auth" state={{showRegister: false}} className="login-btn"
+                      style={{textDecoration: 'none', height: "fit-content"}}>Login</Link>
             </div>
         </div>
     );
