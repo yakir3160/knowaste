c2d96ee34eb19ef5d35608dc48ba28e6e7155848
Raz <raz.erlich4@gmail.com>
2024-11-09 10:43:40 +0200

    Fix formatting in firebaseConfig.js and secure config values
    Corrected spacing inconsistencies in firebaseConfig.js to enhance code readability. Replaced hard-coded Firebase configuration values with environment variables for improved security.

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,22 +1,18 @@
-
-import {initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth,GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
-
 const firebaseConfig = {
-    apiKey: 'AIzaSyCvGGpbmVpf-jZTNsClUDgDMURcP4zxxB4',
-    authDomain: "knowwaste-f88b2.firebaseapp.com",
-    databaseURL: "https://knowwaste-f88b2-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "knowwaste-f88b2",
-    storageBucket: "knowwaste-f88b2.appspot.com",
-    messagingSenderId: "347995591885",
-    appId: "1:347995591885:web:c9d2d38e6b4b374dc70584",
-    measurementId: "G-DGP0BW2Z0X"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
@@ -26,4 +22,4 @@ const db = getFirestore(app);
 
 console.log("Firebase initialized");
 console.log(auth.currentUser);
-export { auth, googleProvider,db, analytics };
+export { auth, googleProvider, db, analytics };
