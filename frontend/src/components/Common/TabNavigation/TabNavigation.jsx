5df0a915021ee13a308a6c700c1fcb1ab09f421c
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2024-11-26 16:21:15 +0200

    -Updated the tab component to be more modular in that it accepts an array of tab names and a callback function that updates the tab change.

--- frontend/src/components/Common/TabNavigation/TabNavigation.jsx
+++ frontend/src/components/Common/TabNavigation/TabNavigation.jsx
@@ -1,25 +1,33 @@
-import React, {useState} from 'react';
-  const TabNavigation =({tabs,onTabChange})=>{
-        const [activeTab,setActiveTab]=useState(tabs[0]);
-        const handleTabChange=(tabName) =>
-        {
-            setActiveTab(tabName);
-            onTabChange(tabName);
-        };
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-      return(
-         <div className={`w-fit bg-base border border-secondary  self-center rounded-sm shadow-inset-custom `}>
-             {tabs.map((tabName) => (
-                 <button key={tabName} className={`px-4 py-4 rounded-sm font-semibold
-                        ${activeTab === tabName ? 'bg-secondary  text-buttons shadow-outer-custom' : ''}
-                        transition-colors duration-150 `}
-                        onClick={() => handleTabChange(tabName)}
-                 >
-                        {tabName}
-                 </button>
-             )
-             )}
-         </div>
-      );
-  }
-  export default TabNavigation;
+const TabNavigation = ({ tabs = ['tab1', 'tab2'] ,onTabChange}) => {
+    TabNavigation.propTypes = {
+        tabs: PropTypes.arrayOf(PropTypes.string)
+    };
+    const [activeTab, setActiveTab] = useState(tabs[0] || '');
+
+    const handleTabChange = (tabName) => {
+        setActiveTab(tabName);
+        onTabChange(tabName);
+    };
+
+    return (
+        <div className={`w-fit bg-base border border-secondary self-center rounded-sm shadow-inset-custom`}>
+            {tabs?.map((tabName) => (
+                <button
+                    key={tabName}
+                    className={`px-4 py-4 rounded-sm font-semibold
+                        ${activeTab === tabName ? 'bg-secondary text-buttons shadow-outer-custom' : ''}
+                        transition-colors duration-150`}
+                    onClick={() => handleTabChange(tabName)}
+                >
+                    {tabName}
+                </button>
+            ))}
+        </div>
+    );
+};
+
+
+export default TabNavigation;
