4aee3729088db77847fb5a1087939d2bd1e6eb34
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2024-12-10 12:23:07 +0200

    -I updated the Add Component form to be compatible with an existing component and a new component.

--- frontend/src/components/AdminPanel/Shared/InventoryManagement/Menu/IngredientForm.jsx
+++ frontend/src/components/AdminPanel/Shared/InventoryManagement/Menu/IngredientForm.jsx
@@ -37,7 +37,7 @@ const IngredientForm = ({ index, isEditing, prefix = "" }) => {
     };
 
     return (
-        <div className="space-y-6 p-6 bg-white rounded-lg shadow-lg">
+        <div className="space-y-6 p-6 bg-white rounded-lg ">
             <Button
                 onClick={handleNewProductToggle}
                 className={`w-full ${isNewProduct ? 'bg-lime ' : 'bg-white '} border border-lime`}
